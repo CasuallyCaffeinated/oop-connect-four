264a9813127fab023ac905e0715c7071844282f1
ondre <ondre.williams.77@gmail.com>
2021-02-11 13:38:49 -0800

    start1

---
+++ updateState.js
@@ -0,0 +1,14 @@
+class UpdateState{
+    constructor(token, ){
+
+    }
+}
+
+// If column full - cant click and show message
+// If column not full - hover event listener red or black
+
+
+// Track filled squares and columns
+// See if game is won
+// Start new game
+//
